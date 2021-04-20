6a2361b5aa6dc74d73d9d79c447cf77940f08196
fmihel <fmihel@gmail.com>
2021-04-20 15:47:29 +0300

    order-test: release

--- server/after_update.php
+++ server/after_update.php
@@ -5,9 +5,10 @@
  * Выполнение идет пошагово. 
  * В начале нужно запросить кол-во шагов
  * https://windeco.su/remote_access_api/wu/server/after_update.php?key=kdiun78js&count
+ * http://windeco/wu/server/after_update.php?key=test&count
  * Выполнение шага:
  * https://windeco.su/remote_access_api/wu/server/after_update.php?key=kdiun78js&step=NNN
- * windeco/wu/server/after_update.php?key=kdiun78js&step=NNN
+ * windeco/wu/server/after_update.php?key=test&step=NNN
  */
 
     
@@ -19,83 +20,82 @@ require_once 'createTree.php';
 require_once 'createTreeFull.php';
 require_once 'tree_generate.php';
 require_once 'arch.php';
+require_once 'order_test.php';
 
 $out = array('res'=>0);
+
+//--------------------------------------------------------------------
 // кол-во шагов
-$COUNT_STEPS = 5;
 
-    
-
+//--------------------------------------------------------------------
 if (isset($_REQUEST['count'])){
+    $COUNT_STEPS = 5 + ORDER_TEST::count();
     echo $COUNT_STEPS;
     exit;
 };
-
-
-
+//--------------------------------------------------------------------
 $catalogJsPath = __DIR__.WS_CONF::GET('catalogJsPath');
-
+//--------------------------------------------------------------------
+        
 if (isset($_REQUEST['step'])){
+    
     $step = intval($_REQUEST['step']);
     
-    //шаги 
-    switch ($step){
-        case 0:
-            $out = CREATE_TREE_UTILS::SAVE_ALL($catalogJsPath);
-            break;
-        case 1:
-            $file = $catalogJsPath.'/catalog_last_update_date.php';
-
-            file_put_contents($file,
-'<?php 
+    // шаги 
+    //--------------------------------------------------------------------    
+    if ($step == 0){
+        
+        $out = CREATE_TREE_UTILS::SAVE_ALL($catalogJsPath);
+    //--------------------------------------------------------------------    
+    }elseif($step == 1){
+        
+        $file = $catalogJsPath.'/catalog_last_update_date.php';
+        file_put_contents($file,
+    '<?php 
     /*данный файл генерируется автоматически, из скрипта after_update.php*/
     define("CATALOG_LAST_UPDATE_DATE","'.date('d/m/Y H:i').'");
     define("CATALOG_JS_CACHE","'.md5(file_get_contents($catalogJsPath.'/catalog.js')).'");
-?>'
-            );
-            $out['res'] = 1;
-            break;
-        case 2:
-            // очистка кеша BUFFER
-            $q = 'truncate table BUFFER';
-            if (base::query($q,'deco'))
-                $out['res'] = 1;
-            break;
-        case 3:
-
-            try{
-                TREE_GENERATE::create($catalogJsPath.'/catalog_new.js');
-            }catch(Exception $e){
-            }
-            $out['res'] = 1;
-            break;
-        case 4:
-            
-            //archAll::$path = '../../archAll/tmp/';
-            //archAll::$catalogPath = '../../createTree/catalog.js';
-            //archAll::$mediaPath = '../../../../';  
-            //archAll::$mediaHttp = 'http://windeco.su/';  
-            //archAll::$zipPath = '../../../../download/catalog.zip';  
-            arch::$path          = WS_CONF::GET('arch_path');
-            arch::$catalogPath   = WS_CONF::GET('arch_catalogPath');
-            arch::$mediaPath     = WS_CONF::GET('arch_mediaPath');  
-            arch::$mediaHttp     = WS_CONF::GET('arch_mediaHttp');  
-            arch::$zipPath       = WS_CONF::GET('arch_zipPath');  
-
-            arch::create();
-            //echo arch::debug_info();
+    ?>'
+    );
+        $out['res'] = 1;
     
-            
+    //--------------------------------------------------------------------
+    }elseif($step == 2){
+        // очистка кеша BUFFER
+        $q = 'truncate table BUFFER';
+        if (base::query($q,'deco'))
             $out['res'] = 1;
-            break;
-    }
-    
+    //--------------------------------------------------------------------
+    }elseif($step == 3){
+        try{
+            TREE_GENERATE::create($catalogJsPath.'/catalog_new.js');
+        }catch(Exception $e){
+        }
+        $out['res'] = 1;
+    //--------------------------------------------------------------------
+    }elseif($step == 4){
+        //archAll::$path = '../../archAll/tmp/';
+        //archAll::$catalogPath = '../../createTree/catalog.js';
+        //archAll::$mediaPath = '../../../../';  
+        //archAll::$mediaHttp = 'http://windeco.su/';  
+        //archAll::$zipPath = '../../../../download/catalog.zip';  
+        arch::$path          = WS_CONF::GET('arch_path');
+        arch::$catalogPath   = WS_CONF::GET('arch_catalogPath');
+        arch::$mediaPath     = WS_CONF::GET('arch_mediaPath');  
+        arch::$mediaHttp     = WS_CONF::GET('arch_mediaHttp');  
+        arch::$zipPath       = WS_CONF::GET('arch_zipPath'); 
+        arch::create();
+        //echo arch::debug_info();
+        
+        $out['res'] = 1;
+    //--------------------------------------------------------------------
+    }elseif($step>4){
+        ORDER_TEST::step($step-5);
+        $out['res'] = 1;
+    };
+    //--------------------------------------------------------------------
 };
 
-
-if ($out['res'] === 1)
-    echo RESULT_OK;
-else
-    echo RESULT_ERROR;
-
+echo ( $out['res'] === 1 ? RESULT_OK : RESULT_ERROR );
+    
 ?>
---
+++ server/order_test.php
@@ -0,0 +1,269 @@
+<?php
+
+class session {
+    static $data = [
+        'ID_USER'=>-1,
+        'PRICE_VIEW_TYPE'=>1,
+        'KOEF_JALUZI'=>1,
+        'ID_DEALER'=>21039
+    ];
+    static $enable = true;
+}
+
+WS_CONF::DEF('require_order_test',[
+    '../../dev/source/common/order_const.php',
+    '../../dev/source/tables/tables.php',
+    '../../dev/source/common/common.php',
+    '../../dev/source/common/order.php',
+    '../../dev/source/common/orders.php',
+    '../../dev/source/common/order_jaluzi.php',
+    '../../dev/source/common/jaluzi.php',
+]);
+WS_CONF::DEF('emails-for-system-report',['fmihel76@gmail.com']);// куда отправлять, если ошибка (пока в заказе жалюзи)
+
+$require_order_test = WS_CONF::GET('require_order_test');
+foreach($require_order_test as $file)
+    require_once $file;
+
+
+class ORDER_TEST{
+    
+    public static function count(){
+        try {
+            return base::valE('select count(ID_ORDER) from ORDERS where FOR_TEST=1',0,'deco');
+        } catch (\Exception $e) {
+            error_log('Exception ['.__FILE__.':'.__LINE__.'] '.$e->getMessage());
+        };
+        return 0;
+    }
+    public static function step($i){
+
+        try {
+
+            $orders = base::rowsE('select * from ORDERS where FOR_TEST =1 order by ID_ORDER','deco');
+            $order = $orders[$i];
+            $test = self::test($order);
+
+            if (COMMON::get($test,'result','msg','') != '' ){
+
+                $tests[]=array_merge(['ID_ORDER'=>$order['ID_ORDER']],$test['result']);
+                $xml = ARR::to_json($tests,true,0,['left'=>'&nbsp;&nbsp;&nbsp;&nbsp;','cr'=>'<br>']);
+                
+                COMMON_UTILS::sendReportToAdmin([
+                    'msg'=>$xml
+                ]);
+            };
+       
+
+        } catch (\Exception $e) {
+            error_log('Exception ['.__FILE__.':'.__LINE__.'] '.$e->getMessage());
+        };
+    }
+    private static function test($param=[]){
+        try {
+            $out = [];
+
+            $p = array_merge([  
+                'ID_ORDER'=>6992,
+                'ID_ORDER_KIND'=>3
+            ],$param);
+            
+            if (!$p['ID_ORDER']){
+                throw new Exception("need ID_ORDER in ");
+            }
+
+            // создаем копию заказа ---------------------
+            $copy = ORDER::crCopy($p['ID_ORDER']);
+            if ($copy['res']==0)
+                throw new Exception("ошибка создания копии");
+            
+            // пересчитываем копию -----------------------    
+            if ($p['ID_ORDER_KIND'] == 3){ // для жалюзи
+                $original = ORDER::load($p['ID_ORDER'])['products'];
+                $data = ORDER::load($copy['ID_ORDER']);
+                $reculc = ORDER::update($data,['reculc'=>1])['data'];
+                // сравниваем оригинал и копию ---------------
+                $eqResult = self::jeqProducts($original,$reculc);
+                $out = ['original'=>$original[0]['ctrl'],'reculc'=>$reculc[0]['ctrl'],'result'=>$eqResult];
+            };
+            // удаляем копию -----------------------------
+            ORDER::delete($copy['ID_ORDER']);
+
+            return array_merge(['res'=>1],$out);
+
+        } catch (\Exception $e) {
+            error_log('Exception ['.__FILE__.':'.__LINE__.'] in ORDER_TEST::test('.print_r($param,true).') '.$e->getMessage());
+            return ['res'=>0,'msg'=>$e->getMessage()];
+        };
+        return ['res'=>0];
+        
+    }
+
+    /**  сравнение изделий */
+    private static function jeqProducts($from,$to){
+        
+            $res = [];
+            $msg = '';
+            try {
+                for($i=0;$i<count($from);$i++){
+                    
+                    $stepMsg = '';
+                    $product = [
+                        'TEMPLATE_NAME' =>base::valE('select NAME from J_FOLDER where ID='.$from[$i]['panel']['ID_J_FOLDER'],'undef','deco','UTF-8'),  
+                        'ID_J_PRODUCT'  =>$from[$i]['panel']['ID_J_PRODUCT'],
+                        'ID_J_FOLDER'   =>$from[$i]['panel']['ID_J_FOLDER'],
+                    ];
+                    //error_log(print_r($from,true));
+                    $out = self::jeqProduct($from[$i],$to[$i]);
+                    if ($out!==[]) {
+                        if (count($out['out'])){
+                            if (!isset($product['ctrls']))
+                                $product['ctrls']=[];
+                            $product['ctrls']= array_merge($product['ctrls'],$out['out']);
+                        }
+                        $stepMsg.=$out['msg'];
+                    }
+    
+                    $tovarFrom = ORDER_JALUZI::getProductTovars($from[$i]['panel']['ID_J_PRODUCT']);
+                    $tovarTo = ORDER_JALUZI::getProductTovars($to[$i]['panel']['ID_J_PRODUCT']);
+                    $eqTov = self::jeqTovars($tovarFrom['data'],$tovarTo['data']);
+    
+                    if ( count($eqTov['out'])>0 || $eqTov['msg']!='' ){
+                        if (!isset($product['tovars']))
+                                $product['tovars']=[];
+                        $product['tovars']=array_merge($product['tovars'],$eqTov['out']);
+                        $stepMsg.=$eqTov['msg'];
+                    }
+                    if ($stepMsg!=''){
+                        $res[]=$product;
+                        $msg.=$stepMsg;
+                    }
+                }
+                return ['products'=>$res,'msg'=>$msg];
+            } catch (\Exception $e) {
+                error_log('Exception ['.__FILE__.':'.__LINE__.'] '.$e->getMessage());
+                return ['products'=>$res,'msg'=>$e->getMessage()];
+            };
+            
+            
+    }
+    /**  сравнение двух изделий */
+    private static function jeqProduct($from,$to){
+            $out = [];
+            $msg = '';
+            try {
+                // сравниваем по панелям
+                if ($from['panel']['PROD_ITOG']['value']!=$to['panel']['PROD_ITOG']['value']){
+                    $msg.='конечная стоимость PROD_ITOG ('.$from['panel']['PROD_ITOG']['value'].'!='.$to['panel']['PROD_ITOG']['value'].'), ';
+                }
+                // сравниваем по компонентам
+                for($i=0;$i<count($from['ctrl']);$i++){
+                    $tc = false;
+                    for($j=0;count($to['ctrl']);$j++){
+                        if ($to['ctrl'][$j]['id'] == $from['ctrl'][$i]['id']){
+                            $tc = $to['ctrl'][$j];
+                            break;
+                        }
+                    }
+                    
+                    if ($tc){
+                        $res = self::jeqCtrl($from['ctrl'][$i],$tc);
+    
+                        if ($res){
+                            $out[]=['from'=>$res['from'],'to'=>$res['to']];
+                            $msg .= $res['msg'];
+                        }
+                    }
+                }
+                return ['out'=>$out,'msg'=>$msg];
+            } catch (\Exception $e) {
+                error_log('Exception ['.__FILE__.':'.__LINE__.'] '.$e->getMessage());
+                return ['out'=>$out,'msg'=>$e->getMessage()];
+            };
+            
+    }
+    /** сравнение компонентов */
+    private static function jeqCtrl($from,$to){
+            $format = function ($paramName,$msg='') use ($from,$to) {
+                return [
+                    'msg'=>$from['caption'].' ID_J_ITEM('.$from['ID_J_ITEM'].'/'.$to['ID_J_ITEM'].') '.$msg.' '.$paramName.'('.$from[$paramName].'!='.$to[$paramName].'), ',
+                    'from'=>$from,
+                    'to'=>$to
+                ];
+            };
+    
+            if ( $from['visible'] == $to['visible'] ){
+                if ( $from['define']!=='price' )
+                    return false;
+            }else
+                return $format('visible');   
+    
+            if ($from['id']!==$to['id']){
+                return $format('id','разные компоненты');
+            }
+    
+            if ($from['type']!==$to['type']){
+                return $format('type','разные типы компонентов');
+            }
+    
+            if ($from['type']=='combo'){
+                return false;
+            }else{
+                if ($from['value']!==$to['value'])
+                    return $format('value','значение');
+            };
+            return false;
+    }
+    /** сравнение по товарам */
+    private static function jeqTovars($from,$to){
+            $out = [];
+            $msg = '';
+
+            $check = function ($table,$ft,$find,$paramName,$text='') use ($from,$to,&$out,&$msg) {
+                $fromValue  = $table::val($ft,$paramName);
+                $toValue = $table::val($find,$paramName);
+                if ($fromValue != $toValue){
+                    $out[] = [
+                        'from'=>$from,
+                        'to'=>$to
+                    ];
+                    $msg.= $text.' ID_J_RESULT='.$table::val($ft,'ID_J_RESULT').' '.$paramName.'('.$fromValue.'!='.$toValue.'), ';
+                };
+            };
+
+            try {
+                if (count($from)!=count($to)){
+                    return ['out'=>['from'=>$from,'to'=>$to],'msg'=>'разное кол-во товаров,'];
+                }
+        
+                for($i = 0;$i<count($from);$i++){
+                    $ft = $from[$i];
+                    $find = false;
+                    for($j = 0;$j<count($to);$j++){
+                        $tt = $to[$j];
+                        if ($tt['ID_TOVAR'] == $ft['ID_TOVAR']){
+                            $find=$tt;
+                            break;
+                        }
+                    }
+                    if (!$find)
+                        return ['out'=>['from'=>$from,'to'=>$to],'msg'=>'товары не совпадают,'];
+                    
+                    $check('J_RESULT_TABLE',$ft,$find,'CNT');
+                    $check('TOVAR_TABLE',$ft,$find,'NAME');
+                    $check('TOVAR_TABLE',$ft,$find,'COLOR');
+                    $check('TOVAR_TABLE',$ft,$find,'ART');
+                    $check('COD_TABLE',$ft,$find,'COD');
+                    $check('COD_TABLE',$ft,$find,'FULL_NAME');
+                    
+                }
+                return ['out'=>$out,'msg'=>$msg];
+                    
+            } catch (\Exception $e) {
+                error_log('Exception ['.__FILE__.':'.__LINE__.'] '.$e->getMessage());
+                return ['out'=>[],'msg'=>$e->getMessage()];
+            };
+            
+    }
+}
+?>
