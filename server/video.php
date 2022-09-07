235eff3fd3338d0fe04b8bbb5666b4a6d30c89f7
fmihel <fmihel76@gmail.com>
2022-09-07 09:37:38 +0300

    fix video, recreate js tree catalog

--- server/video.php
+++ server/video.php
@@ -10,6 +10,8 @@ $videoPath = WS_CONF::GET('videoPath');
 $videoUrl =  WS_CONF::GET('videoUrl');
 $updatePath = WS_CONF::GET('UPDATE_ZIP_PATH');
 
+$reCreateCatalogJsUrl  = WS_CONF::GET('url:after_update.php').'?key='.WS_CONF::GET('key').'&step=3';
+
 try{
     /** проверка существования записи */
     if (Common::issets($_REQUEST,'exists','ID_C_MEDIA_FILE')){
@@ -24,6 +26,7 @@ try{
     /** перемещение файла и запись его текущего пути в таблицу */
     if (Common::issets($_REQUEST,'reg','path','file','ID_C_MEDIA_FILE')){
         
+        $ID_C_MEDIA_FILE = $_REQUEST['ID_C_MEDIA_FILE'];
         if ($ID_C_MEDIA_FILE>0){
             $q = "select count(ID_C_MEDIA_FILE) cnt from C_MEDIA_FILE where ID_C_MEDIA_FILE = $ID_C_MEDIA_FILE";
             $cnt = base::value($q,'cnt',0,'deco');
@@ -52,6 +55,17 @@ try{
             $q = "insert into C_MEDIA_FILE (ID_C_MEDIA_FILE,PATH_WWW,PROCESSING_KIND,CAPTION) values ($ID_C_MEDIA_FILE,'$PATH_WWW',4,'$file') on duplicate key update PATH_WWW='$PATH_WWW'";
             base::queryE($q,'deco','utf8');
         };
+
+
+        //------------------------------------------------------
+        // запуск скрипта, на пересоздание дерева catalog_new.js
+        $ch = curl_init($reCreateCatalogJsUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $str = curl_exec($ch);
+        error_log('call '.$reCreateCatalogJsUrl.' res='.$str);
+        curl_close($ch);
+
+        //------------------------------------------------------
 
         echo RESULT_OK;
         exit;
